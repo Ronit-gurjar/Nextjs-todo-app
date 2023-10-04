b55ccf52767b009d3226bfdfd8462c805e5552d8
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-10-05 01:59:03 +0530

    Add: input form with states and submit btn

--- app/components/Addtask.tsx
+++ app/components/Addtask.tsx
@@ -2,16 +2,32 @@
 
 import {AiOutlinePlus} from 'react-icons/ai'
 import Modal from './Modal'
-import {useState} from 'react';
+import {FormEventHandler,useState} from 'react';
 
 const Addtask = () => {
 
   const [modalOpen , setmodalOpen] = useState(false);
+  const [newTaskValue, setnewTaskValue] = useState('');
+
+  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> =(e)=>{
+    e.preventDefault();
+    console.log("new task created:",newTaskValue);
+    setnewTaskValue('');
+  }
+
   return (
     <div>
     <button onClick={()=> setmodalOpen(true)} className='btn btn-primary w-full'>Add new task<AiOutlinePlus size={20}/></button>
 
-    <Modal modalOpen={modalOpen} setmodalOpen={setmodalOpen}/>
+    <Modal modalOpen={modalOpen} setmodalOpen={setmodalOpen}>
+    <form onSubmit={handleSubmitNewTodo} method="dialog">
+      <h3 className="font-bold text-lg">Create task</h3>
+      <div className="modal-action">
+      <input value={newTaskValue} onChange={(e)=> setnewTaskValue(e.target.value)} type="text" placeholder="Type here" className="input input-bordered input-primary w-full" />
+      <button type="submit" className='btn btn-primary'>Submit</button>
+      </div>
+    </form>
+    </Modal>
     </div>
   )
 }
