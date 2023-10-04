afeb669c608a734fef28a1692c275d45eba29428
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-10-04 17:35:46 +0530

    Add: Addtask to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Addtask from "./components/Addtask";
 
 
 export default function Home() {
@@ -5,7 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen max-w-2xl mx-auto flex-col items-center justify-between mt-4">
       <div className="text-center my-2 flex flex-col gap-2">
         <h1 className="font-bold">todo app</h1>
-        <button className="btn">click me</button>
+        <Addtask/>
       </div>
     </main>
   )
