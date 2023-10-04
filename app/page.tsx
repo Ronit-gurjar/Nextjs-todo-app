15c011560e9001a18b2e6edd5135e91a1cb9094b
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-10-05 01:40:10 +0530

    Fix: margin on text

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import Todolist from "./components/Todolist";
 export default async function Home() {
   const task = await getAllTodos();
   return (
-    <main className="flex min-h-screen max-w-2xl mx-auto flex-col items-center justify-start mt-4">
-      <div className="text-center my-2 flex flex-col gap-2">
+    <main className="flex min-h-screen max-w-2xl mx-auto flex-col items-center justify-start">
+      <div className="text-center my-2 flex flex-col gap-2 mt-4">
         <h1 className="text-3xl font-bold">todo app</h1>
         <Addtask/>
       </div>
