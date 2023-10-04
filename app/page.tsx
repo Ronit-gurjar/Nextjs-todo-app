88100ea89e56b89d1c7bbffe06b441a5fa5abd43
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-10-04 17:27:02 +0530

    Create: basic ui with title and btn

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>todo app</h1>
+    <main className="flex min-h-screen max-w-2xl mx-auto flex-col items-center justify-between mt-4">
+      <div className="text-center my-2 flex flex-col gap-2">
+        <h1 className="font-bold">todo app</h1>
+        <button className="btn">click me</button>
+      </div>
     </main>
   )
 }
