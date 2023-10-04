4d27733ed27add83a58b002fd87034d4e642279a
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-10-04 18:52:43 +0530

    Add: table for task

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Addtask from "./components/Addtask";
+import Todolist from "./components/Todolist";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen max-w-2xl mx-auto flex-col items-center justify-between mt-4">
+    <main className="flex min-h-screen max-w-2xl mx-auto flex-col items-center justify-start mt-4">
       <div className="text-center my-2 flex flex-col gap-2">
         <h1 className="text-3xl font-bold">todo app</h1>
         <Addtask/>
       </div>
+      <Todolist/>
     </main>
   )
 }
