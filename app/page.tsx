e7dc0460e0298b7cda0171888227aff60fccf61b
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-10-04 18:24:16 +0530

    fix: typo

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen max-w-2xl mx-auto flex-col items-center justify-between mt-4">
       <div className="text-center my-2 flex flex-col gap-2">
-        <h1 className="font-bold">todo app</h1>
+        <h1 className="text-3xl font-bold">todo app</h1>
         <Addtask/>
       </div>
     </main>
