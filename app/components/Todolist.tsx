ceb099f489dfad369c133cd32c0c12fc7fa874ae
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-10-06 00:33:10 +0530

    Add: action icons

--- app/components/Todolist.tsx
+++ app/components/Todolist.tsx
@@ -1,5 +1,7 @@
 import { ITask } from '@/types/task'
 import React from 'react'
+import {FiEdit} from 'react-icons/fi'
+import {GoTrash} from 'react-icons/go'
 
 interface TodolistProps{
     tasks : ITask[]
@@ -15,7 +17,7 @@ const Todolist: React.FC<TodolistProps> = ({tasks}) => {
         <th></th>
         <th></th>
         <th>Task</th>
-        
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -28,7 +30,10 @@ const Todolist: React.FC<TodolistProps> = ({tasks}) => {
             </th>
             <th>{task.id}</th>
             <td>{task.text}</td>
-            
+            <td className='flex gap-10'>
+              <FiEdit className='text-blue-400 rounded-lg border-spacing-6 hover:bg-slate-500 hover:text-black hover:border-spacing-6 ' cursor='pointer' size={25}/>
+              <GoTrash className='text-red-600 rounded-lg border-spacing-6 hover:bg-red-700 hover:text-black hover:border-spacing-6' cursor='pointer' size={25}/>
+            </td>
       </tr>
       ))}
     </tbody>
