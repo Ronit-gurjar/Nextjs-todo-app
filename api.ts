8c61eb84b54d40f82badb793c233229d96efc4ce
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-10-05 14:28:28 +0530

    Add: error handling for POST fetch

--- api.ts
+++ api.ts
@@ -9,12 +9,25 @@ export const getAllTodos = async (): Promise<ITask[]> => {
 }
 
 export const addTodo = async (todo: ITask): Promise<ITask> => {
-    const res = await fetch(`${baseUrl}/tasks`,{
-        method:'POST', 
-        headers:{'content-type':'application/json'},
-        body: JSON.stringify(todo)
-    });
-    const newtodos = await res.json();
-    return newtodos;
-}
+    try {
+        const res = await fetch(`${baseUrl}/tasks`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(todo),
+        });
+
+        if (!res.ok) {
+            throw new Error(`Failed to fetch. Status code: ${res.status}`);
+        }
+
+        const newTodo = await res.json();
+        return newTodo;
+    } catch (error) {
+        console.error("Error in addTodo:", error);
+        throw error; // Rethrow the error for further handling
+    }
+};
+
 
